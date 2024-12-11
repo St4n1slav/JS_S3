1124855967d0c1432a74e80921ac51be8cf13e5b
“Stanislav” <stanyyaro@gmail.com>
2024-12-11 16:48:09 +0100

    S3L3

---
+++ S3L3/assets/script/Pratica_L3.js
@@ -0,0 +1,33 @@
+let toDo = []
+function addToDo() {
+    let list = document.querySelectorAll("header input")
+    let obj = {
+        toDo: list[0].value,
+        complete: false
+    }
+    toDo.push(obj);
+    reseteForm(list);
+    todoListCreated();
+    console.log(toDo);
+}
+
+function reseteForm(node) {
+    node[0].value = "";
+}
+
+function todoListCreated() {  
+    let listNode = document.querySelector("main ul");
+    toDo.forEach((ele, i)=>{
+        let li = document.createElement("li");
+        li.innerHTML = `${ele.list} <span><button onclick="removeList(${i})">X</button></span>`
+        listNode.appendChild(li)
+    })
+}
+
+function removeList(index) {
+    toDo.splice(index, 1);
+    todoListCreated();
+}
+
+let button = document.getElementById("addButton");
+button.onclick = addToDo;
